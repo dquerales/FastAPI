2cbbb0fa10832841c6afdd02e24d0f4d0b64af51
unknown <d.querales@gmail.com>
2023-10-08 14:27:40 -0400

    actions: remove pylint for black

--- api.py
+++ api.py
@@ -5,17 +5,14 @@ import pandas as pd
 
 app = FastAPI()
 
-
 class ScoringItem(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
 
-
 with open("model/model.pkl", "rb") as f:
     model = pickle.load(f)
-
 
 @app.post("/")
 async def scoring_endpoint(item: ScoringItem):
